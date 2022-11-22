2d2e3553672ce87291f0f1753b6389f05627bfad
Roman Belov <109288853+gravirovshik@users.noreply.github.com>
2022-11-23 00:16:12 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,13 @@
-/* Это объявление переменной, мы наши кнопку по тегу */
-const button = document.querySelector('button');
+const button = document.querySelector('.button-link');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
+	alert('И кнопочка работает')
+})
+
+const ava = document.querySelector('.header__avatar');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+ava.addEventListener('click', function() {
 	alert('Так работает alert в Java Script')
 })
